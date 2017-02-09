52ecb5f9b60285d6f6ba37b5f7099b4aced693f1
Danny Guinther <danny@zensight.co>
2017-02-09 13:40:15 -0500

    Update react-quill to remove Quill generated toolbar on dismount

--- src/vendor/bundle.min.js
+++ src/vendor/bundle.min.js
@@ -10899,6 +10899,10 @@
 		componentWillUnmount: function() {
 			// NOTE: Don't set the state to null here
 			//       as it would generate a loop.
+
+			// If a Quill generated a toolbar, remove it.
+			var toolbar = this.refs.quill.querySelector(".ql-toolbar");
+			if (toolbar) { this.refs.quill.removeChild(toolbar); }
 		},
 
 		shouldComponentUpdate: function(nextProps, nextState) {
@@ -10979,7 +10983,8 @@
 				className: ['quill'].concat(this.props.className).join(' '),
 				onKeyPress: this.props.onKeyPress,
 				onKeyDown: this.props.onKeyDown,
-				onKeyUp: this.props.onKeyUp },
+				onKeyUp: this.props.onKeyUp,
+				ref: 'quill'},
 				this.renderContents()
 			);
 		},
